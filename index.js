9a556f3c7ebf87fd189b0341fb2c0928377ff747
dhamolahedonist <dhamolahedonist>
2023-03-05 12:11:56 +0100

    webhook route added

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 
-const axios = require("axios");
-const path = require("path");
 const PORT = 3000;
 app.use(express.static("static"));
 app.use(express.json());
@@ -17,17 +15,28 @@ app.get("/", (req, res) => {
   });
 });
 
+app.post("/webhook", (req, res) => {
+  console.log(req.body);
+  bot.telegram.sendMessage(
+    req.body?.chat?.id,
+    "Hello there! welcome to Jumia foods, webhook reached successfully"
+  );
+  res.json({
+    status: true,
+  });
+});
+
 bot.command("start", (ctx) => {
-  console.log(ctx.from);
+  console.log(ctx.chat);
   bot.telegram.sendMessage(
     ctx.chat.id,
-    "Hello there! welcome to Jumia foods, Please note tht we are currently under maintenance!!"
+    "Hello there! welcome to Jumia foods, Please note tht we are currently under maintenance"
   );
 });
 
-bot.launch();
-// app.use(bot.webhookCallback('/webhook'))
-// bot.telegram.setWebhook(`${process.env.BASE_URL}/webhook`)
+// bot.launch();
+app.use(bot.webhookCallback("/webhook"));
+bot.telegram.setWebhook(`${process.env.BASE_URL}/webhook`);
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);

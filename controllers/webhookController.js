31ea4bcd8fc2f01b576aa3000c67f2e79936cc9c
dhamolahedonist <dhamolahedonist>
2023-03-05 13:11:37 +0100

    webhookController route updated

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -1,6 +1,6 @@
 const webhookController = {
   handle: (req) => {
-    return "Hello there! welcome to Jumia foods, webhook reached successfully";
+    return `Hi ${req.body.message.chat.first_name}, welcome back! \n\n1 to place an order \n99 to checkout order \n98 to see order history \n97 to see current order \n0 to cancel order`;
   },
 };
 

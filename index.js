35f91e2e55ec82363225f342aff8a96fa9188d7b
dhamolahedonist <dhamolahedonist>
2023-03-11 23:34:30 +0100

    update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/webhook", async (req, res) => {
-  let reply = "I cannot understand your request, Please try again";
+  let reply = "Please press 1";
   try {
     reply = await webhookController.handle(req);
   } catch (error) {
